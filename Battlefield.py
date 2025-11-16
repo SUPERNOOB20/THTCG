f82f162ebe1d63d46952c98a54a2708fd31a3890
SUPERNOOB20 <andycia802@gmail.com>
2025-11-16 20:11:40 -0300

    loads music for gaem :3

--- Battlefield.py
+++ Battlefield.py
@@ -1,1 +1,4 @@
-c:\Users\Claudia\AppData\Local\Microsoft\Windows\Fonts\CC Wild Words Italic.ttf
+import pygame
+
+pygame.mixer.music.load(filename = "Sinesita - UHC T9 - 05 Persimmon.wav", namehint = "battlefield_0")
+pygame.mixer.music.load(filename = "Sinesita - UHC T5 - Morfeo.wav", namehint = "battlefield_1")
